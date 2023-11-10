beff2549c04320d630a1d271d2607cd36ba0111e
artLara <art.lara.caz.com>
2023-11-09 21:25:44 -0600

    Tensorflow ommited

--- classifier/views.py
+++ classifier/views.py
@@ -6,9 +6,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import json
 import requests
+
+from classifier.service.src.FingerSpellingService import FingerSpellingService
+
+fingerSpellingService = FingerSpellingService()
+
+
 class Post_APIView(APIView):
     def get(self, request, format=None, *args, **kwargs):
         data = json.loads(request.body)
+        # message = fingerSpellingService.getPhrase(jsonFile=data)
+        # payload = {'message': message}
+        # r = requests.get('http://myserver/emoncms2/api/post', data=payload)
         print(data)
 
     def post(self, request, format=None):
